25b3d9587c751af8a6696f8be7edaf480663218e
IMTTeixeira <20.01681-6@maua.br>
2022-06-08 11:08:32 -0300

    Aula13-Pokemons

---
+++ Teoria/Aula13-Estudo/Estudo/src/Status.java
@@ -0,0 +1,39 @@
+public class Status {
+    private int vida;
+    private int ataque;
+    private int defesa;
+    private int velocidade;
+
+    public Status(int vida, int ataque, int defesa, int velocidade) {
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+        this.velocidade = velocidade;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public int getDefesa() {
+        return defesa;
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Status{" +
+                "vida=" + vida +
+                ", ataque=" + ataque +
+                ", defesa=" + defesa +
+                ", velocidade=" + velocidade +
+                '}';
+    }
+}
