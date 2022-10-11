80f0facf6c45f5d5a7bbc6860e1971f033e70a06
IMTTeixeira <20.01681-6@maua.br>
2022-10-11 19:09:29 -0300

    Detalhes

--- Atividade3/src/home.py
+++ Atividade3/src/home.py
@@ -31,13 +31,7 @@ def criar_produtos(product):
         button = st.button("Adicionar ao carrinho", key = product.url)
         if button:
             st.session_state["carrinho"].addProduct(product)
-            if product.name == "The Beginning After the End":
-                Carrinho.q1 = quantidade
-            elif product.name == "One Piece":
-                Carrinho.q2 = quantidade
-            elif product.name == "Solo Leveling":
-                Carrinho.q3 = quantidade
-            # st.session_state["quantidade"] = quantidade
+            st.session_state["quantidade"] = quantidade
               
 if "login" not in st.session_state:
     st.session_state.login = False
