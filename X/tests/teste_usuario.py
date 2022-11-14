a88fd723ea316856190ce423698efcf228110f92
IMTTeixeira <20.01681-6@maua.br>
2022-11-14 17:20:03 -0300

    Corrigindo erro

---
+++ X/tests/teste_usuario.py
@@ -0,0 +1,4 @@
+from src.controllers.usuario_controller import UsuarioController
+from src.models.usuario import Usuario
+
+usuario1 = Usuario(nome="Zed", email = "zed@email.com", senha="deathmark")
