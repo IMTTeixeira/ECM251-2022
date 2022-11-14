45b694722baa951172a7c341b6a249d5cf4caed8
IMTTeixeira <20.01681-6@maua.br>
2022-11-14 19:43:00 -0300

    Login, Cadastro e Configurações funcionando

--- X/tests/teste_produto.py
+++
@@ -1,5 +0,0 @@
-from src.controllers.produto_controller import *
-from src.models.produto import *
-from src.dao.produto_dao import *
-
-produto1 = ProdutoController.cadastrar_produto(produto(id=1, nome="The Beginning After the End", preco=99.99, imagem="https://mangazim.com/wp-content/uploads/2022/09/the-beginning-after-the-end-todos-os-capitulos-mangazim.jpg"))
